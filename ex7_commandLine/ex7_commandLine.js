fa4dedce13c6af619fe767cba97929d3df3bbc9c
Alex <al.vaccaro@outlook.com>
2024-04-17 12:48:47 +0200

    esercizio 7 command line svolto

---
+++ ex7_commandLine/ex7_commandLine.js
@@ -0,0 +1,10 @@
+var figlet = require("figlet");
+
+figlet("Benvenuto!", function (err, data) {
+  if (err) {
+    console.log("Qualcosa è andato storto...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
